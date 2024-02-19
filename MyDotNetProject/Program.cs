5667e68f6a456250c6975f5422e43c85375c5268
rachitpoudel101 <rachit300poudel@gmail.com>
2024-02-19 19:14:53 +0545

    Learn about the dotnet

---
+++ MyDotNetProject/Program.cs
@@ -0,0 +1,28 @@
+﻿// using System;
+
+// class Program
+// {
+    
+//     static void Main(string[] args)
+//     {
+//         int age = 30;
+//         double salary = 1000.50;
+//         string name = "John";
+//         bool isEmployed = true;
+//         // Console.WriteLine("Hello mY NAME Is Rachit");
+    
+
+//     if (age >= 18)
+//     {
+//         Console.WriteLine("You are an adult.");
+//     }
+//     else
+//     {
+//         Console.WriteLine("You are a minor.");
+//     }
+
+
+//     }
+// }
+
+
