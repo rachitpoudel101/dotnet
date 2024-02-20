0dfd424b05745ac06a5585c647151e4a5a0e283f
rachitpoudel101 <rachit300poudel@gmail.com>
2024-02-20 18:04:34 +0545

    Learn about the different way to printing in the console in C#.

--- MyDotNetProject/Program.cs
+++ MyDotNetProject/Program.cs
@@ -1,28 +1,28 @@
-﻿// using System;
+﻿using System;
 
-// class Program
-// {
+class Program
+{
     
-//     static void Main(string[] args)
-//     {
-//         int age = 30;
-//         double salary = 1000.50;
-//         string name = "John";
-//         bool isEmployed = true;
-//         // Console.WriteLine("Hello mY NAME Is Rachit");
+    static void Main(string[] args)
+    {
+        int age = 30;
+        double salary = 1000.50;
+        string name = "John";
+        bool isEmployed = true;
+        // Console.WriteLine("Hello mY NAME Is Rachit");
     
 
-//     if (age >= 18)
-//     {
-//         Console.WriteLine("You are an adult.");
-//     }
-//     else
-//     {
-//         Console.WriteLine("You are a minor.");
-//     }
-
-
-//     }
-// }
-
-
+        if (age >= 18)
+        {
+            Console.WriteLine("You are an\t adult.");// \t sequence will add a tab.
+        }
+        else
+        {
+            Console.WriteLine("You are a minor."+"Your age:\n"+ age);
+        }
+        Console.WriteLine(name);
+        //If We use Console.Write() multiple times consecutively, it will keep appending the output without any separation or new line between the subsequent calls.
+        Console.Write(salary);
+        Console.Write(isEmployed );
+    }
+}
--- MyDotNetProject/Rachit.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-class Rachit
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, world!");
-    }
-}
